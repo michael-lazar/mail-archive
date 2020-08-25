8ddefa5e022077a110984f57471f142067927714
Michael Lazar <mlazar@doctorondemand.com>
2020-08-25 00:40:20 -0400

    Scrape command take 2

--- mail_archive/custom/management/commands/scrape_web_archive.py
+++ mail_archive/custom/management/commands/scrape_web_archive.py
@@ -1,16 +1,13 @@
 """
 Scrape a mailing list archive over HTTP and import into hyperkitty.
 
-Usage:
-    python manage.py scrape_web_archive \
-        https://lists.orbitalfox.eu/archives/gemini/ \
-        --list-address gemini@lists.orbitalfox.eu
-
 Copyright Michael Lazar 2020.
 """
+
 import gzip
 import os
 import tempfile
+import time
 
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
@@ -39,33 +36,41 @@ class Command(BaseCommand):
         resp = requests.get(archive_url)
         resp.raise_for_status()
 
-        update_index = False
+        matches = lxml.html.fromstring(resp.content).xpath('//a/@href')
+        links = [str(m) for m in matches if str(m).endswith(self.FILE_EXTENSIONS)]
 
-        html = lxml.html.fromstring(resp.content)
-        for match in html.xpath('//a/@href'):
-            link = str(match)
-            if link.endswith(self.FILE_EXTENSIONS):
-                self.stdout.write(f"Found archive {link}, downloading...")
-                update_index = True
+        if not options['full']:
+            # Take only the 2 most recent archives, which should ensure that
+            # everything is captured since the last scrape.
+            links = links[:2]
 
-                url = os.path.join(archive_url, link)
-                resp = requests.get(url)
-                resp.raise_for_status()
+        mbox_files = []
 
-                mbox_data = resp.content
-                if link.endswith('.gz'):
-                    self.stdout.write("Decompressing gzip archive")
-                    mbox_data = gzip.decompress(resp.content)
+        for link in links:
+            self.stdout.write(f"Found archive {link}, downloading...")
+            url = os.path.join(archive_url, link)
+            resp = requests.get(url)
+            resp.raise_for_status()
 
-                with tempfile.NamedTemporaryFile() as fp:
-                    fp.write(mbox_data)
-                    fp.flush()
+            mbox_data = resp.content
+            if link.endswith('.gz'):
+                self.stdout.write("Decompressing gzip archive")
+                mbox_data = gzip.decompress(resp.content)
 
-                    list_address = options['list_address']
-                    call_command("hyperkitty_import", fp.name, list_address=list_address)
+            with tempfile.NamedTemporaryFile(delete=False) as fp:
+                mbox_files.append(fp.name)
+                fp.write(mbox_data)
 
-                if not options['full']:
-                    break
+            # Rate limit
+            time.sleep(1)
 
-        if update_index:
-            call_command("update_index_one_list", list_address)
+        command_kwargs = {'list_address': options['list_address']}
+        if options['full']:
+            command_kwargs['since'] = '1980-01-01'
+
+        call_command("hyperkitty_import", *mbox_files, **command_kwargs)
+        call_command("update_index_one_list", options['list_address'])
+
+        self.stdout.write("Cleaning up temporary files")
+        for filename in mbox_files:
+            os.remove(filename)
